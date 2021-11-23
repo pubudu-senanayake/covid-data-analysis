abcd4f50f789317fa5a4925a03e69baf7dd3ebb4
Noobyon <pubudu.sen@gmail.com>
2021-11-23 17:40:32 +1300

    change date of cases etc to pure dates (rather than date-time fields)

--- src/data_pull.R
+++ src/data_pull.R
@@ -73,6 +73,8 @@ for (state in state_list) {
 }
 
 aus_data <- 
-  aus_data %>% mutate(as_at = Sys.Date())
+  aus_data %>% 
+  mutate(date = as.Date(date),
+         as_at = Sys.Date())
 
 write_csv(aus_data, sprintf("data_push/aus_data.csv"))
